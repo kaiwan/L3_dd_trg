c8847a03bc069061c0353ce0a0eaa218b43a0463
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2020-03-09 15:31:44 +0530

    Disp elapsed time in other units as well
    Signed-off-by: Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>

--- ktime_stuff/time_lld_cookbk/time.c
+++ ktime_stuff/time_lld_cookbk/time.c
@@ -1,8 +1,8 @@
 /*
  * Kernel timing functions
  * (c) Linux Device Drivers Cookbook, Packt
+ * (Minimal enhancements, comments; Kaiwan NB).
  */
-
 #define pr_fmt(fmt) "%s:%s: " fmt, KBUILD_MODNAME, __func__
 #include <linux/module.h>
 #include <linux/delay.h>
@@ -14,7 +14,8 @@
         t0 = ktime_get_real_ns();   \
         code;               \
         t1 = ktime_get_real_ns();   \
-        pr_info(str " -> %11llu ns\n", t1 - t0); \
+        pr_info(str " -> %11llu ns = %5llu us = %3llu ms\n", \
+			          (t1 - t0), (t1-t0)/1000, (t1-t0)/1000000); \
     } while (0)
 
 /*
@@ -23,15 +24,21 @@
 
 static int __init time_init(void)
 {
-    pr_info("*delay() functions:\n");
+	/* Atomic, busy-loops, no sleep! */
+    pr_info("*delay() functions (atomic, in a delay loop):\n");
     print_time("        10 ns via ndelay(10)", ndelay(10));
+	/* Preferred interface is udelay() */
     print_time("    10,000 ns via udelay(10)", udelay(10));
     print_time("10,000,000 ns via mdelay(10)", mdelay(10));
 
-    pr_info("*sleep() functions:\n\n");
+	/* Non-atomic, blocking APIs; causes schedule() to be invoked */
+    pr_info("*sleep() functions (process ctx, sleeps/schedule()'s out):\n\n");
+	/* usleep_range(): HRT-based; for approx range [10us - 20ms] */
     print_time("        10,000 ns via usleep_range(10,10)", usleep_range(10, 10));
+	/* msleep(): jiffies/legacy-based; for longer sleeps (> 10ms) */
     print_time("    10,000,000 ns via          msleep(10)", msleep(10));
     print_time("    10,000,000 ns via     msleep_intr(10)", msleep_interruptible(10));
+	/* ssleep() is a wrapper over msleep():  = msleep(ms*1000); */
     print_time("10,000,000,000 ns via          ssleep(10)", ssleep(10));
 
     return -EINVAL;

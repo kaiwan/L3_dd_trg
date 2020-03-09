ed8f7b39e79e1c103dabbcd4b7a4b52f250e9466
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2020-03-09 15:31:00 +0530

    Retrieve and use the device ptr with dev_dbg()

--- platform_devdrv_demo/platdemo.c
+++ platform_devdrv_demo/platdemo.c
@@ -4,19 +4,21 @@
  * Simple demo platform device and associated platform driver.
  *
  * Setup a bare-bones platform device & associated driver.
- * Platform devices get bound to their driver simply on the basis of the 'name' field;
- * if they match, the driver core "binds" them, invoking the 'probe' routine. Conversely, the
- * 'remove' method is invoked at unload/shutdown.
+ * Platform devices get bound to their driver simply on the basis of the 'name'
+ * field; if they match, the driver core "binds" them, invoking the 'probe'
+ * routine. Conversely, the 'remove' method is invoked at unload/shutdown.
  *
  * License: Dual MIT/GPL
  * Kaiwan NB <kaiwan -at- kaiwantech -dot- com>
  * (c) kaiwanTECH
  */
 #include <linux/init.h>
+#include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/sched.h>
 #include <linux/fs.h>
 #include <linux/slab.h>
+#include <linux/device.h>
 #include <linux/platform_device.h>
 #include "../convenient.h"
 
@@ -36,7 +38,7 @@ static struct platform_device plat0 = {
 	/* But then again, we can use the 'driver_override' member to force
 	 * a match to the driver's name! Then it still works..
 	 */
-	.driver_override = DRVNAME,	/* Driver name to force a match */
+	.driver_override = DRVNAME,	/* Driver name to force a match! */
 #else
 	.name = DRVNAME,
 #endif
@@ -56,9 +58,9 @@ static struct platform_driver platdrv = {
 	.probe = platdev_probe,
 	.remove = platdev_remove,
 	.driver = {
-		   .name = DRVNAME,	// matches platform device name
-		   .owner = THIS_MODULE,
-		   },
+	   .name = DRVNAME,	// matches platform device name
+	   .owner = THIS_MODULE,
+	},
 };
 
 /* Most drivers have a global "context" struct that gets passed around .. */
@@ -75,24 +77,26 @@ typedef struct MyCtx {
 static int platdev_probe(struct platform_device *platdev)
 {
 	pstMyCtx pstCtx = platdev->dev.platform_data;
-	pstCtx->data_xform = 100;
+	struct device *dev = &platdev->dev;
 
-	MSG("Platform driver: probe method invoked\n");
+	pstCtx->data_xform = 100;
+	dev_dbg(dev, "Platform driver: probe method invoked\n");
 	return 0;
 }
 
 static int platdev_remove(struct platform_device *platdev)
 {
 	pstMyCtx pstCtx = platdev->dev.platform_data;
+	struct device *dev = &platdev->dev;
 
-	MSG("Platform driver:remove method invoked\n data_xform=%d\n",
+	dev_dbg(dev, "Platform driver:remove method invoked\n data_xform=%d\n",
 	    pstCtx->data_xform);
 	return 0;
 }
 
 static void plat0_release(struct device *platdev)
 {
-	MSG("Platform device: release method invoked\n");
+	dev_dbg(platdev, "Platform device: release method invoked\n");
 }
 
 static int __init platdev_init(void)
@@ -103,10 +107,8 @@ static int __init platdev_init(void)
 	pr_info("%s: Initializing platform demo driver now...\n", DRVNAME);
 
 	pstCtx = kzalloc(sizeof(stMyCtx), GFP_KERNEL);
-	if (!pstCtx) {
-		pr_alert("%s:%s kzalloc failed!\n", DRVNAME, __func__);
+	if (!pstCtx)
 		return -ENOMEM;
-	}
 	plat0.dev.platform_data = pstCtx;	// convenient to access later
 
 	/* platform_add_devices() is a wrapper over platform_device_register() */
@@ -125,7 +127,7 @@ static int __init platdev_init(void)
 	/* Successful platform_driver_register() will cause the registered 'probe'
 	 * method to be invoked now..
 	 */
-	pr_info("%s loaded.\n", DRVNAME);
+	dev_dbg(&plat0.dev, "loaded.\n");
 	return res;
 
  out_fail_pdr:
@@ -137,18 +139,19 @@ static int __init platdev_init(void)
 
 static void __exit platdev_exit(void)
 {
+	struct device *dev = &plat0.dev;
 	pstMyCtx pstCtx = plat0.dev.platform_data;
 
+	dev_dbg(dev, "unloading\n");
 	platform_driver_unregister(&platdrv);
 	platform_device_unregister(&plat0);
 	kfree(pstCtx);
-	pr_info("%s unloaded.\n", DRVNAME);
 }
 
 module_init(platdev_init);
 module_exit(platdev_exit);
 
 MODULE_DESCRIPTION
-("Simple demo platform driver; can use as a template for a platform device/driver.");
-MODULE_AUTHOR("Kaiwan N Billimoria <kaiwan@kaiwantech.com>");
+("Simple demo platform driver; can use as a template for a platform device/driver");
+MODULE_AUTHOR("Kaiwan N Billimoria");
 MODULE_LICENSE("Dual MIT/GPL");

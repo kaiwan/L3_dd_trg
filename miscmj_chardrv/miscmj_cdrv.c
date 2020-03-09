0f20263cd4d8fa697ae02f08b755ea37b0042f22
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2020-03-09 10:31:40 +0530

    Use the device ptr and dev_dbg()

--- miscmj_chardrv/miscmj_cdrv.c
+++ miscmj_chardrv/miscmj_cdrv.c
@@ -11,43 +11,45 @@
 
 #define MYDRVNAME    "miscdrv"
 
+static struct device *dev;
+
 static int my_dev_open(struct inode *inode, struct file *file)
 {
-	pr_info("%s:%s() called.\n"
+	dev_dbg(dev, "%s() called.\n"
 		" minor # is %d\n",
-		MYDRVNAME, __func__, iminor(inode));
+		__func__, iminor(inode));
 	return 0;
 }
 
 static ssize_t my_dev_read(struct file *file, char __user *buf,
 	size_t count, loff_t *off)
 {
-	pr_info("%s:%s() called.\n"
+	dev_dbg(dev, "%s() called.\n"
 		" request to read %zu bytes\n",
-		MYDRVNAME, __func__, count);
+		__func__, count);
 	return count;
 }
 
 static ssize_t my_dev_write(struct file *file, const char __user *buf,
 	size_t count, loff_t *off)
 {
-	pr_info("%s:%s() called.\n"
+	dev_dbg(dev, "%s() called.\n"
 		" request to write %zu bytes\n",
-		MYDRVNAME, __func__, count);
+		__func__, count);
 	return count;
 }
 
 static int my_dev_close(struct inode *inode, struct file *file)
 {
-	pr_info("%s:%s() called.\n", MYDRVNAME, __func__);
+	dev_dbg(dev, "%s() called.\n", __func__);
 	return 0;
 }
 
 static long my_dev_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
-	pr_info("%s:%s() called.\n"
-		" cmd = %d, arg = %ld\n"
-		, MYDRVNAME, __func__, cmd, arg);
+	dev_dbg(dev, "%s() called.\n"
+		" cmd = %d, arg = %ld\n",
+		__func__, cmd, arg);
 	return 0;
 }
 
@@ -61,16 +63,19 @@ static const struct file_operations my_dev_fops = {
 };
 
 /* declare & initialize struct miscdevice */
+#define DEVNODENM "miscdev"
 static struct miscdevice my_miscdevice = {
 	.minor = MISC_DYNAMIC_MINOR, /* dynamically allocate an available minor # */
-	.name = "mydev",             /* when misc_register() is invoked, the kernel
-	                               will auto-create device file as /dev/mydev */
+	.name = DEVNODENM,           /* when misc_register() is invoked, the kernel
+	                               will auto-create device file as /dev/miscdev */
+	.mode = 0666,                  /* ... with perms set as specified here */
 	.fops = &my_dev_fops,        /* functionality */
 };
 
 static int __init miscdrv_init(void)
 {
 	int ret_val;
+
 	pr_info("%s: misc char driver init\n", MYDRVNAME);
 
 	/* Register the device with the Kernel */
@@ -79,14 +84,14 @@ static int __init miscdrv_init(void)
 		pr_err("%s: could not register the misc device mydev", MYDRVNAME);
 		return ret_val;
 	}
-
-	pr_info("%s: mydev: got minor %i\n", MYDRVNAME, my_miscdevice.minor);
+	dev = my_miscdevice.this_device;
+	dev_dbg(dev, "dev ptr = %pK; device node: /dev/%s\n", dev, DEVNODENM);
 	return 0;
 }
 
 static void __exit miscdrv_exit(void)
 {
-	pr_info("%s: misc char driver exit\n", MYDRVNAME);
+	dev_dbg(dev, "misc char driver exit\n");
 
 	/* Unregister the device with the kernel */
 	misc_deregister(&my_miscdevice);

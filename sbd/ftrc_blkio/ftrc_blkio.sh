89f89ce0fb0bf08296b1fc840952ed205fc3aaed
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2020-03-12 01:58:02 +0000

    Add some event classes, other minor

--- sbd/ftrc_blkio/ftrc_blkio.sh
+++ sbd/ftrc_blkio/ftrc_blkio.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Global settings
-EV_CLASSES="-e block -e fs -e ext4 -e filemap -e vfs -e hda -e scsi"
-FN_NOT_TRACE="__do_page_fault" # -n __do_page_fault"
+EV_CLASSES="-e block -e fs -e ext4 -e filemap -e vfs -e mmc -e hda -e scsi -e iommu -e pagemap"
+#FN_NOT_TRACE="__do_page_fault" # -n __do_page_fault"
 BUFFER_SZ_KB_PCPU=9000
 
 # 'sync' the IO and ftrace it via trace-cmd(1)
@@ -13,8 +13,8 @@ CMD2="sync"
 FTRC_REPORT2=./blk-mmc-report.sync_cmd.txt
 CMD2_RAW_TRC_FILE=cmd2.dat
 
-taskset 01 trace-cmd record ${EV_CLASSES} -v -e *vm* -n ${FN_NOT_TRACE} \
--p function_graph -b ${BUFFER_SZ_KB_PCPU} -i -o ${CMD2_RAW_TRC_FILE} -F ${CMD2}
+taskset 01 trace-cmd record ${EV_CLASSES} -v -e *vm* \
+ -p function_graph -b ${BUFFER_SZ_KB_PCPU} -i -o ${CMD2_RAW_TRC_FILE} -F ${CMD2}
 
 rm -f ${FTRC_REPORT2}
 # -l => latency format
@@ -25,6 +25,10 @@ trace-cmd report -l -i ${CMD2_RAW_TRC_FILE} >> ${FTRC_REPORT2}
 ### 'main'
 [ $(id -u) -ne 0 ] && {
   echo "$0: must run as root"
+  exit 1
+}
+which trace-cmd >/dev/null || {
+  echo "$0: trace-cmd(1) not installed?"
   exit 1
 }
 lsmod |grep -q sbd || {

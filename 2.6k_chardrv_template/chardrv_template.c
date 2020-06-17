ca3fa24fe44e248e00a60717c086108608da5503
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2020-06-17 13:04:51 +0100

    redone to conform to checkpatch errs/warns

--- 2.6k_chardrv_template/chardrv_template.c
+++ 2.6k_chardrv_template/chardrv_template.c
@@ -12,7 +12,7 @@
 
 // copy_[to|from]_user()
 #include <linux/version.h>
-#if LINUX_VERSION_CODE > KERNEL_VERSION(4,11,0)
+#if LINUX_VERSION_CODE > KERNEL_VERSION(4, 11, 0)
 #include <linux/uaccess.h>
 #else
 #include <asm/uaccess.h>
@@ -21,104 +21,110 @@
 #include <linux/cdev.h>
 
 #define	DRVNAME			"chardrv"	// the device nodes will be named /dev/<DRVNAME>.<minor#>
-#define DRV_MINOR_START			0
-#define DEV_COUNT        		2	// # of devices (& device nodes) to create
+#define DRV_MINOR_START		0
+#define DEV_COUNT			2	// # of devices (& device nodes) to create
 #define DEVICE_FILE		"/dev/cdrv"
 
 #ifdef DEBUG
-	#define MSG(string, args...) \
- 	   printk(KERN_DEBUG "%s:%s:%d: " string, \
-  	   DRVNAME, __FUNCTION__, __LINE__, ##args)
+#define MSG(string, args...) \
+	printk(KERN_DEBUG "%s:%s:%d: " string, \
+		DRVNAME, __func__, __LINE__, ##args)
 #else
-	#define MSG(string, args...)
+#define MSG(string, args...)
 #endif
 
 dev_t chardrv_dev_number;
 struct chardrv_dev {
 	char name[10];
-	struct cdev cdev;     /* Char device structure      */
+	struct cdev cdev;	/* Char device structure */
 } *chardrv_devp;
-struct class *chardrv_class=NULL;
+struct class *chardrv_class;
 
 /* Minor-specific open routines */
-static struct file_operations drv_fops = {
-	.llseek =       no_llseek,
+static const struct file_operations drv_fops = {
+	.llseek = no_llseek,
 	/* Add char device f_ops above this line */
 };
 
-static int cdrv_open(struct inode * inode, struct file * filp)
+static int cdrv_open(struct inode *inode, struct file *filp)
 {
-	MSG( "Device node with minor # %d being used\n", iminor(inode));
-
+	MSG("Device node with minor # %d being used\n", iminor(inode));
 	switch (iminor(inode)) {
-		case 0:
-			filp->f_op = &drv_fops;
-			break;
-		default:
-			return -ENXIO;
+	case 0:
+		filp->f_op = &drv_fops;
+		break;
+	default:
+		return -ENXIO;
 	}
 	if (filp->f_op && filp->f_op->open)
-		return filp->f_op->open(inode,filp);
+		return filp->f_op->open(inode, filp);
 	return 0;
 }
 
 /* Major-wide open routine */
-static struct file_operations cdrv_open_fops = {
-	.open =         cdrv_open, /* just a means to get at the real open */
+static const struct file_operations cdrv_open_fops = {
+	.open = cdrv_open, /* just a means to get at the real open */
 };
 
 /*--- Dynamic Char Device Registration & device nodes creation---------*/
 static int chardev_registration(void)
 {
-    int res=0,i=0;
+	int res = 0, i = 0;
 
-	res = alloc_chrdev_region(&chardrv_dev_number, DRV_MINOR_START, DEV_COUNT, DEVICE_FILE);
+	res =
+	    alloc_chrdev_region(&chardrv_dev_number, DRV_MINOR_START, DEV_COUNT,
+				DEVICE_FILE);
 	if (res) {
 		pr_warn("%s: could not allocate device\n", DRVNAME);
 		return res;
-	} else {
-		pr_info("%s: registered with major number %d\n", DRVNAME, MAJOR(chardrv_dev_number));
 	}
+	pr_info("%s: registered with major number %d\n", DRVNAME,
+			 MAJOR(chardrv_dev_number));
 
-	chardrv_devp = kzalloc(DEV_COUNT * sizeof(struct chardrv_dev), GFP_KERNEL);
-	if (NULL == chardrv_devp) {
+	chardrv_devp =
+	    kzalloc(DEV_COUNT * sizeof(struct chardrv_dev), GFP_KERNEL);
+	if (chardrv_devp == NULL) {
 		pr_warn("%s: kzalloc failed!\n", DRVNAME);
 		return -ENOMEM;
 	}
 
-	for (i=0 ; i<DEV_COUNT; i++) {
+	for (i = 0; i < DEV_COUNT; i++) {
 		cdev_init(&chardrv_devp[i].cdev, &cdrv_open_fops);
 		chardrv_devp[i].cdev.owner = THIS_MODULE;
 		chardrv_devp[i].cdev.ops = &cdrv_open_fops;
-		res = cdev_add (&chardrv_devp[i].cdev, MKDEV(MAJOR(chardrv_dev_number), 
-			   MINOR(chardrv_dev_number)+i), 1);
+		res =
+		    cdev_add(&chardrv_devp[i].cdev,
+			     MKDEV(MAJOR(chardrv_dev_number),
+				 MINOR(chardrv_dev_number) + i), 1);
 		if (res) {
-			pr_notice("%s: Error on cdev_add for %d\n", DRVNAME, res);
+			pr_notice("%s: Error on cdev_add for %d\n", DRVNAME,
+				   res);
 			return res;
-		} else {
-			pr_info("%s: cdev %s.%d added\n", DRVNAME, DRVNAME, i);
 		}
+		pr_info("%s: cdev %s.%d added\n", DRVNAME, DRVNAME, i);
 	}
 
 	/* Create the devices.
-	 * !Note! : APIs class_create, device_create, etc exported as EXPORT_SYMBOL_GPL(...); 
+	 * !Note! : APIs class_create, device_create, etc exported as EXPORT_SYMBOL_GPL(...);
 	 * so will not show up unless the module license is GPL.
 	 */
 	chardrv_class = class_create(THIS_MODULE, DRVNAME);
-	for ( i=0 ; i<DEV_COUNT; i++) {
-		if (!device_create(chardrv_class, NULL, MKDEV(MAJOR(chardrv_dev_number), 
-		      MINOR(chardrv_dev_number)+i), NULL, "%s.%d", DRVNAME, i)) {
-			pr_notice("%s: Error creating device node /dev/%s.%d !\n", 
-				DRVNAME, DRVNAME, i);
+	for (i = 0; i < DEV_COUNT; i++) {
+		if (!device_create
+		     (chardrv_class, NULL,
+		      MKDEV(MAJOR(chardrv_dev_number),
+			  MINOR(chardrv_dev_number) + i), NULL, "%s.%d",
+		      DRVNAME, i)) {
+			pr_notice
+			    ("%s: Error creating device node /dev/%s.%d !\n",
+			     DRVNAME, DRVNAME, i);
 			return res;
 		}
-		else {
-			pr_info(" %s: Device node /dev/%s.%d created.\n", DRVNAME, DRVNAME, i);
-		}
+		pr_info(" %s: Device node /dev/%s.%d created.\n",
+				 DRVNAME, DRVNAME, i);
 	}
 	return res;
 }
-
 
 static int __init cdrv_init_module(void)
 {
@@ -127,12 +133,13 @@ static int __init cdrv_init_module(void)
 
 static void __exit cdrv_cleanup_module(void)
 {
-	int i=0;
-
-	/* Char driver unregister */
-	for (i=0; i<DEV_COUNT; i++) {
+	int i = 0;
+    /* Char driver unregister */
+	for (i = 0; i < DEV_COUNT; i++) {
 		cdev_del(&chardrv_devp[i].cdev);
-		device_destroy(chardrv_class, MKDEV(MAJOR(chardrv_dev_number), MINOR(chardrv_dev_number)+i));
+		device_destroy(chardrv_class,
+			MKDEV(MAJOR(chardrv_dev_number),
+		    MINOR(chardrv_dev_number) + i));
 	}
 	class_destroy(chardrv_class);
 	kfree(chardrv_devp);
@@ -142,11 +149,11 @@ static void __exit cdrv_cleanup_module(void)
 
 module_init(cdrv_init_module);
 module_exit(cdrv_cleanup_module);
-
 MODULE_AUTHOR("Your Name here");
 MODULE_DESCRIPTION("Write a one-line description of the char device driver here.");
+
 #if 1
-MODULE_LICENSE("GPL"); /* necessary for the device API */
+MODULE_LICENSE("GPL");	/* necessary for the device API */
 #endif
 /*
  If not released under license "GPL", we get these errors upon insmod:

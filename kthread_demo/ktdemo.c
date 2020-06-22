4fb45eef4fbc3c2c1f3620701cbab92e9a4d83f2
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2020-06-22 17:17:08 +0530

    add pr_fmt(), minor updt

--- kthread_demo/ktdemo.c
+++ kthread_demo/ktdemo.c
@@ -3,6 +3,8 @@
  * Simple demo of creating and using a kernel thread.
  * (c) Kaiwan NB, kaiwanTECH
  */
+#define pr_fmt(fmt) "%s:%s(): " fmt, KBUILD_MODNAME, __func__
+
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/sched.h>
@@ -18,27 +20,30 @@ struct task_struct *gKthrd;
 /* Our simple kernel thread. */
 static int mykt(void *arg)
 {
-	MSG("name: %s PID: %d TGID: %d\n",
+	pr_info("name: %s PID: %d TGID: %d\n",
 		current->comm, current->pid, current->tgid);
 	if (!current->mm)
-		MSG("mm field NULL.\n");
+		pr_info("mm field NULL.\n");
 
-	// By default all signals are masked for the kthread; allow a couple
-	// so that we can kill it
+	/* By default all signals are masked for the kthread; allow a couple
+	 * so that we can 'manually' kill it
+	 */
 	allow_signal(SIGINT);
 	allow_signal(SIGQUIT);
 
 	while (1) {
-		MSG("K Thread %d going to sleep now...\n", current->pid);
+		pr_info("K Thread %d going to sleep now...\n", current->pid);
 		set_current_state (TASK_INTERRUPTIBLE);
 		schedule();	// and yield the processor...
-		// we're back on! has to be due to either the SIGINT or SIGQUIT signal!
+		/* we're back on! here, it has to be due to either the SIGINT
+ 		 * or SIGQUIT signal! */
 		if (signal_pending(current))
 			break;
 	}
+
 	// We've been interrupted by a signal...
 	set_current_state (TASK_RUNNING);
-	MSG("K thread %d exiting now...\n", current->pid);
+	pr_info("K thread %d exiting now...\n", current->pid);
 	do_exit(0);
 	BUG(); // do_exit() should never return
 	return 0;
@@ -48,7 +53,12 @@ static int ktdemo_init(void)
 {
 	int ret=0, i=0;
 
-	MSG("Create a kernel thread...\n");
+	pr_info("Create a kernel thread...\n");
+
+	/*
+	 * kthread_run(threadfn, data, namefmt, ...)
+	 * - it's a thin wrapper over the kthread_create() API
+	 */
 	gKthrd = kthread_run(mykt, NULL, "%s.%d", MODULE_NAME, i);
 		/* 2nd arg is (void * arg) to pass, ret val is task ptr on success */
 	if (ret < 0) {
@@ -57,10 +67,10 @@ static int ktdemo_init(void)
 	}
 	get_task_struct(gKthrd); // inc refcnt, "take" the task struct
 
-	MSG("Module %s initialized, thread task ptr is 0x%pK.\n"
+	pr_info("Module %s initialized, thread task ptr is 0x%pK (actual=0x%llx)\n"
 	"See the new k thread '%s.0' with ps "
 	"(and kill it with SIGINT or SIGQUIT)\n", 
-		MODULE_NAME, gKthrd, MODULE_NAME);
+		MODULE_NAME, gKthrd, (unsigned long long)gKthrd, MODULE_NAME);
 	return 0;	// success
 }
 
@@ -69,7 +79,7 @@ static void ktdemo_exit(void)
 	if(gKthrd) 
 		kthread_stop(gKthrd);
 	put_task_struct(gKthrd); // dec refcnt, "release" the task struct
-	MSG("Module %s unloaded.\n", MODULE_NAME);
+	pr_info("Module %s unloaded.\n", MODULE_NAME);
 }
 
 MODULE_LICENSE("Dual MIT/GPL");

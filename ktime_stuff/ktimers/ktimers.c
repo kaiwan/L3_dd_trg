35a04b78eebff06090e25e7b779fd5e7186f594d
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2020-12-02 10:14:57 +0530

    Bug fix

--- ktime_stuff/ktimers/ktimers.c
+++ ktime_stuff/ktimers/ktimers.c
@@ -5,6 +5,8 @@
  * (c) Author: Kaiwan NB
  * License: MIT/GPL
  */
+#define pr_fmt(fmt) "%s:%s(): " fmt, KBUILD_MODNAME, __func__
+
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
@@ -29,8 +31,7 @@ static void ding(struct timer_list *timr)
 	struct kdata *mykd = from_timer(mykd, timr, tmr);
 
 	PRINT_CTX();
-	pr_info("%s:%s()! timed out... data=%d\n",
-		OURMODNAME, __func__, mykd->data--);
+	pr_info("timed out... data=%d\n", mykd->data--);
 
 	/* until countdown done, fire it again! */
 	if (mykd->data)
@@ -41,14 +42,17 @@ static int __init ktimers_init(void)
 {
 	int stat = 0;
 
-	pr_info("%s: inserted\n", OURMODNAME);
+	pr_info("inserted\n");
+
 	kd.data = 114;
+	kd.tmr.expires = jiffies + msecs_to_jiffies(exp_ms);
+    kd.tmr.flags = 0;
 	timer_setup(&kd.tmr, ding, 0);
 
-	pr_info("%s: timer set to expire in %ld ms\n", OURMODNAME, exp_ms);
+	pr_info("timer set to expire in %ld ms\n", exp_ms);
 	stat = mod_timer(&kd.tmr, jiffies + msecs_to_jiffies(exp_ms));
 	if (stat != 0) {
-		pr_warn("%s: mod_timer failed (%d)\n", OURMODNAME, stat);
+		pr_warn("mod_timer failed (%d)\n", stat);
 		return stat;
 	}
 
@@ -57,9 +61,9 @@ static int __init ktimers_init(void)
 
 static void __exit ktimers_exit(void)
 {
-	pr_info("%s: wait for possible timeouts to complete...\n", OURMODNAME);
+	pr_info("wait for possible timeouts to complete...\n");
 	del_timer_sync(&kd.tmr);
-	pr_info("%s: removed\n", OURMODNAME);
+	pr_info("removed\n");
 }
 
 module_init(ktimers_init);

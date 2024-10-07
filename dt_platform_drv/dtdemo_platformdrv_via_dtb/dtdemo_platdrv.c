04284bfa6af560475fdca55eef2bd64e951e35cf
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2024-10-07 14:28:05 +0000

    comment + get the integer property as well

--- dt_platform_drv/dtdemo_platformdrv_via_dtb/dtdemo_platdrv.c
+++ dt_platform_drv/dtdemo_platformdrv_via_dtb/dtdemo_platdrv.c
@@ -19,7 +19,7 @@
  *        dtdemo_chip {
  *                compatible = "dtdemo,dtdemo_platdev";
  *                aproperty = "my prop 1";
- *                reg = <0x1 0x2>;
+ * 		  my_value = <14>;
  *                status = "okay";
  *        };
  * ...
@@ -41,6 +41,38 @@
  * - this causes a ,atch! and voila, the bus driver invokes the driver's probe
  *   routine, and we're on the way to driving it!
  *
+ * ---------------------------------------------
+ * For the BBB - Beagle Bone Black
+ * ---------------------------------------------
+ * 1. Setup the DTBO to get oarsed at boot:
+ *    - Will have to boot via eMMC internal flash (as the /boot stuff's there)
+ *    - compile the DTS to the DTBO (DT overlay blob)
+ *    - copy it into the appropriate location: /boot/dtbs/5.10.168-ti-r79/overlays/BB-BONE-DTDEMO-01.dtbo
+ *    - insert this line into /boot/uEnv.txt to enable our DTBO to get loaded up on boot:
+ *      dtb_overlay=/boot/dtbs/5.10.168-ti-r79/overlays/BB-BONE-DTDEMO-01.dtbo
+ * 2. Boot via eMMC flash (remove the uSD card & boot)
+ * 3. U-Boot prints should show that our DTBO got loaded
+ *    ...
+ *    uboot_overlays: loading /boot/dtbs/5.10.168-ti-r79/overlays/BB-HDMI-TDA998x-00A0.dtbo ...
+ *   5321 bytes read in 6 ms (865.2 KiB/s)
+ *   uboot_overlays: [dtb_overlay=/boot/dtbs/5.10.168-ti-r79/overlays/BB-BONE-DTDEMO-01.dtbo] ...
+ *   uboot_overlays: loading /boot/dtbs/5.10.168-ti-r79/overlays/BB-BONE-DTDEMO-01.dtbo ...
+ *   432 bytes read in 6 ms (70.3 KiB/s)
+ *   loading /boot/initrd.img-5.10.168-ti-r79 ...
+ *   ...
+ *   After getting a shell:
+ *    $ cat /proc/device-tree/my_device/compatible
+ *    dtdemo,dtdemo_platdev
+ * 4. Compile and run the driver; the probe method gets called, priving it worked!
+ *    sudo insmod ././dtdemo_platdrv.ko
+ *    dmesg
+ *    ...
+ *    [  185.450304] dtdemo_platdrv:dtdemo_platdrv_init(): inserted
+ *    [  185.450591] dtdemo_platdev my_device: platform driver probe enter
+ *    [  185.450612] dtdemo_platdev my_device: DT property 'aproperty' = my prop 1 (len=10)
+ *  Done!
+ *
+ * ---------------------------------------------
  * Kaiwan N Billimoria, kaiwanTECH
  * License: Dual MIT/GPL
  */
@@ -73,6 +105,7 @@ int dtdemo_platdev_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
 	const char *prop = NULL;
+	s32 myval;
 	int len = 0;
 
 	dev_dbg(dev, "platform driver probe enter\n");
@@ -80,10 +113,18 @@ int dtdemo_platdev_probe(struct platform_device *pdev)
 	if (pdev->dev.of_node) {
 		prop = of_get_property(pdev->dev.of_node, "aproperty", &len);
 		if (!prop) {
-			dev_warn(dev, "getting DT property failed\n");
+			dev_warn(dev, "getting DT property 'aproperty' failed\n");
 			return -1;
 		}
 		dev_info(dev, "DT property 'aproperty' = %s (len=%d)\n", prop, len);
+
+		// Note that the ret value isn't the property value..(the 3rd param is)
+		len = of_property_read_s32(pdev->dev.of_node, "my_value", &myval);
+		if (len < 0) {
+			dev_warn(dev, "getting DT property 'my_value' failed\n");
+			return -1;
+		}
+		dev_info(dev, "DT property 'my_value' = %d\n", myval);
 	} else {
 		dev_warn(dev, "couldn't access DT property!\n");
 		return -EINVAL;
